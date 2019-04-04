88a5a219118e44352100d38ebe0f48b00e2f367e
Sergio Comerón <sergiocomeron@icloud.com>
2019-04-04 17:27:09 +0200

    Add option to choose how long before you can access a session

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_jitsi';
-$plugin->version = 2019032000;
-$plugin->release = 'v1.3';
+$plugin->version = 2019040400;
+$plugin->release = 'v1.4';
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_STABLE;
