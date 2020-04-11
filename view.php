ebf2edd80101ef005bebc66946184289c0fde3cf
Sergio Comerón <sergiocomeron@icloud.com>
2020-04-11 12:55:30 +0200

    Code corrections

--- view.php
+++ view.php
@@ -74,7 +74,7 @@ if ($jitsi->intro) {
 }
 
 $moderation = false;
-if (has_capability('mod/jitsi:moderation', $context)){
+if (has_capability('mod/jitsi:moderation', $context)) {
     $moderation = true;
 }
 
@@ -91,7 +91,7 @@ $sessionoptionsparam = ['$course->shortname', '$jitsi->id', '$jitsi->name'];
 $fieldssesionname = $CFG->jitsi_sesionname;
 
 $allowed = explode(',', $fieldssesionname);
-$max = sizeof($allowed);
+$max = count($allowed);
 
 $sesparam = '';
 $optionsseparator = ['.', '-', '_', ''];
