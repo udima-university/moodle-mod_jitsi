21dd4c825b2bcb2e61b2dd3d2fdd8438101e58cf
Sergio Comerón <sergiocomeron@icloud.com>
2020-04-13 19:13:05 +0200

    new version

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_jitsi';
-$plugin->version = 2020040900;
+$plugin->version = 2020041300;
 $plugin->release = 'v2.3';
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_STABLE;
