6d5c127eb5edb399130ea5b917cdb42b9b94858a
Samuel CALEGARI <samuel.calegari@gmail.com>
2020-06-15 16:33:26 +0200

    added support for french language

---
+++ lang/fr/jitsi.php
@@ -0,0 +1,85 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for jitsi
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod_jitsi
+ * @copyright  2019 Sergio Comerón Sánchez-Paniagua <sergiocomeron@icloud.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'Jitsi';
+$string['modulenameplural'] = 'Jitsis';
+$string['modulename_help'] = 'Utilisez le module Jitsi pour la vidéoconférence. Ces visioconférences utiliseront votre nom d\'utilisateur Moodle en affichant votre nom d\'utilisateur et votre avatar dans les visioconférences.
+
+Jitsi-meet est une solution de visioconférence open source qui vous permet de créer et de mettre en œuvre facilement des solutions de visioconférence sécurisées.';
+$string['jitsi:addinstance'] = 'Ajouter une instance Jitsi';
+$string['jitsi:moderation'] = 'Jitsi Modération';
+$string['jitsi:sharedesktop'] = 'Partager le bureau';
+$string['jitsi:view'] = 'Voir Jitsi';
+$string['jitsiname'] = 'Nom de session';
+$string['jitsi'] = 'Jitsi';
+$string['pluginadministration'] = 'Administration Jitsi';
+$string['pluginname'] = 'Jitsi';
+$string['instruction'] = 'Cliquez sur le bouton pour accéder';
+$string['access'] = 'Accès';
+$string['calendarstart'] = 'La visioconférence \'{$a}\' commence';
+$string['allow'] = 'Début de la visioconférence';
+$string['nostart'] = 'La session n\'a pas commencé. Vous pouvez accéder {$a} minutes avant le commencement';
+$string['server'] = 'Serveur Jitsi';
+$string['serverexpl'] = 'URL du Serveur Jitsi';
+$string['privacy:metadata:jitsi'] = 'Pour s\'intégrer à une session Jitsi, les données utilisateur doivent être échangées avec ce service.';
+$string['privacy:metadata:jitsi:username'] = 'The username is sent from moodle to show to the other users of the Jitsi session';
+$string['privacy:metadata:jitsi:avatar'] = 'Le nom d\'utilisateur est envoyé par moodle aux autres utilisateurs durant la session Jitsi';
+$string['noviewpermission'] = 'Vous n\'êtes pas autorisé à afficher cette session Jitsi';
+$string['minpretime'] = 'Minutes pour accèder';
+$string['help'] = 'Aide';
+$string['helpex'] = 'Instruction text for show at Jitsi activity';
+$string['identification'] = 'ID Utilisateur';
+$string['identificationex'] = 'ID à afficher durant la session';
+$string['username'] = 'Nom Utilisateur';
+$string['nameandsurname'] = 'Prénom + Nom';
+$string['tokennconfig'] = 'Configuration du token (jeton d\'accès)';
+$string['tokenconfigurationex'] = 'Vide pour les serveurs sans token';
+$string['separator'] = 'Séparateur';
+$string['separatorex'] = 'Définissez le séparateur de champ pour le nom de la session';
+$string['sessionnamefields'] = 'Champs de nom de session';
+$string['sessionnamefieldsex'] = 'Champs qui définissent le nom de la session';
+$string['infoicon'] = 'Icône Info';
+$string['infoiconex'] = 'Afficher l\'icône d\'informations sur la session';
+$string['appid'] = 'App_ID';
+$string['appidex'] = 'App ID pour la configuration du token';
+$string['secret'] = 'Mot de passe';
+$string['secretex'] = 'Mot de passe pour la configuration du token';
+$string['simultaneouscameras'] = 'Caméras simultanées';
+$string['simultaneouscamerasex'] = 'Nombre de Caméras simultanées';
+$string['streamingbutton'] = 'Streaming Youtube';
+$string['streamingbuttonex'] = 'Afficher l\'option de streaming. (uniquement pour les modérateurs)';
+$string['blurbutton'] = 'Option de floutage';
+$string['blurbuttonex'] = 'Afficher l\'option de floutage';
+$string['youtubebutton'] = 'Option de partage Youtube';
+$string['youtubebuttonex'] = 'Afficher l\'option de partage Youtube';
+$string['watermarklink'] = 'Lien filigrane';
+$string['watermarklinkex'] = 'Lien filigrane';
+$string['finishandreturn'] = 'Terminer et Retourner';
+$string ['finishandreturnex'] = 'Retour au cours à la fin de la session';
