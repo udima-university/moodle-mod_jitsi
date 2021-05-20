1fa8d585d729b88f20b8da18185ad801f634256b
sergiocomeron <sergiocomeron@icloud.com>
2021-05-20 16:38:58 +0200

    Session embeded

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_jitsi';
-$plugin->version = 2021051400;
-$plugin->release = 'v2.9.0';
+$plugin->version = 2021052003;
+$plugin->release = 'v2.8.4';
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_STABLE;
