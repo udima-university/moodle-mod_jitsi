4828bf7a81930bdd876907f89ed7a34a57a7bed6
Essoh Lathe Gnagne Sebastien <83341250+atamabou@users.noreply.github.com>
2022-07-10 23:29:39 +0000

    Update session.php

--- session.php
+++ session.php
@@ -40,9 +40,6 @@ if ($state == null) {
     $session = required_param('ses', PARAM_TEXT);
     $avatar = $CFG->jitsi_showavatars == true ? required_param('avatar', PARAM_TEXT) : null;
     $teacher = required_param('t', PARAM_BOOL);
-    $groupeNameSeleted = '';
-    $groupeName = '';
-    $mygrouproomselected = $_POST['mygrouproom'];
 
 } else {
     $paramdecode = base64urldecode($state);
@@ -69,6 +66,13 @@ if ($state == null) {
     $teacher = $teachera[1];
     $stateses = $statesesa[1];
 }
+// start jitsi-group-room parameters
+    $groupeNameSeleted = '';
+    $mygrouproomselected = $_POST['mygrouproom'];
+    $urlparams1 = array('avatar' => $avatar, 'nom' => $nombre, 'ses' => $session, 'userid' => $USER->id, 'courseid' => $courseid, 'cmid' => $cmid, 't' => $teacher);
+    $choosegroup = get_string('access', 'jitsi');
+    $nogroup = get_string('nogroup', 'group');
+// end jitsi-group-room parameters
 
 $cm = get_coursemodule_from_id('jitsi', $cmid, 0, false, MUST_EXIST);
 $course = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
@@ -107,22 +111,12 @@ echo "setInterval(participating, 60000);\n";
 echo "</script>";
 
 // start jitsi-group-room
-
-$results = $DB->get_records_sql("SELECT * FROM {groups_members} gm JOIN {groups} g
-                                    ON g.id = gm.groupid
-                                  WHERE gm.userid = ? AND courseid='$courseid'
-                                   ORDER BY name ASC", array($USER->id));							   
-
-$row_cnt = count($results);
-if($row_cnt > 1){
-
-$urlparams1 = array('avatar' => $avatar, 'nom' => $nombre, 'ses' => $session, 'userid' => $USER->id,
-    'courseid' => $courseid, 'cmid' => $cmid, 't' => $teacher);
-$choosegroup = get_string('access', 'jitsi');
-$nogroup = get_string('nogroup', 'group');
+echo '<script>function changemygrouproom() {document.getElementById("SelectGroupRoomForm").submit();} </script>';	
+if($teacher){
+$results = $DB->get_records_sql("SELECT * FROM {groups} WHERE courseid='$courseid' ORDER BY name ASC");
 echo '<div style="text-align:center;">
 <form name="SelectGroupRoomForm" action="'.new moodle_url('/mod/jitsi/session.php', $urlparams1).'" method="POST" id="SelectGroupRoomForm">
-<select class="custom-select" type="text" name="mygrouproom" id="mygrouproom_ID" style="font-size:16px;" onchange="changemygrouproom()">
+<select class="custom-select" type="text" name="mygrouproom" id="mygrouproom_ID" onchange="changemygrouproom()">
 <option value="" disabled selected>'.$choosegroup.'</option>
 <option value="" >'.$nogroup.'</option>';
 foreach ($results as $row) {
@@ -130,26 +124,33 @@ foreach ($results as $row) {
 echo'<option value="'.$groupeNameSeleted.'">'.$groupeNameSeleted.'</option>';
     }
 echo '</select></form></div>'; 
-echo '<script>function changemygrouproom() {
-  document.getElementById("SelectGroupRoomForm").submit();} </script>';	
-	
-
-	$resultsuite = $DB->get_records_sql("SELECT * FROM {groups_members} gm JOIN {groups} g
+	createsession($teacher, $cmid, $avatar, $nombre, $session.' '.$mygrouproomselected, null, $jitsi);
+}else{
+	$results = $DB->get_records_sql("SELECT * FROM {groups_members} gm JOIN {groups} g
                                     ON g.id = gm.groupid
-                                  WHERE gm.userid = ? AND courseid='$courseid' AND name='$mygrouproomselected' 
-                                   ORDER BY name ASC", array($USER->id));	
-	foreach ($resultsuite as $rowsuite) {
-	$groupeName = $rowsuite->name;
+                                  WHERE gm.userid = ? AND courseid='$courseid'
+                                   ORDER BY name ASC", array($USER->id));
+$row_cnt = count($results);
+if($row_cnt > 1){
+echo '<div style="text-align:center;">
+<form name="SelectGroupRoomForm" action="'.new moodle_url('/mod/jitsi/session.php', $urlparams1).'" method="POST" id="SelectGroupRoomForm">
+<select class="custom-select" type="text" name="mygrouproom" id="mygrouproom_ID" onchange="changemygrouproom()">
+<option value="" disabled selected>'.$choosegroup.'</option>';
+foreach ($results as $row) {
+	$groupeNameSeleted = $row->name;
+echo'<option value="'.$groupeNameSeleted.'">'.$groupeNameSeleted.'</option>';
     }
-	createsession($teacher, $cmid, $avatar, $nombre, $session.' '.$groupeName, null, $jitsi);
+echo '</select></form></div>'; 
+
+	createsession($teacher, $cmid, $avatar, $nombre, $session.' '.$mygrouproomselected, null, $jitsi);
 }
 elseif ($row_cnt <= 1) {
 foreach ($results as $row) {
-	$groupeName = $row->name;
+	$groupeNameSeleted = $row->name;
     }
-	createsession($teacher, $cmid, $avatar, $nombre, $session.' '.$groupeName, null, $jitsi);
+	createsession($teacher, $cmid, $avatar, $nombre, $session.' '.$groupeNameSeleted, null, $jitsi);
 }
-
+}
 // end jitsi-group-room
 
 echo $OUTPUT->footer();
