17b9b54394144c0353463410837112de173c68d2
SergioComeron <sergiocomeron@icloud.com>
2022-07-27 17:10:22 +0200

    new version 3.2.12

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_jitsi';
-$plugin->version = 2022070602;
-$plugin->release = 'v3.2.11';
+$plugin->version = 2022072700;
+$plugin->release = 'v3.2.12';
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_STABLE;
