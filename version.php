102fb34acfa28150ee253de4280096e2ebd8d99c
SergioComeron <sergiocomeron@icloud.com>
2023-11-17 11:57:52 +0100

    new build (2023111701)

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_jitsi';
-$plugin->version = 2023111700;
+$plugin->version = 2023111701;
 $plugin->release = 'v3.3.9';
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_STABLE;
